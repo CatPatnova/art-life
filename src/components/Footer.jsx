bd315da4eb5e428a10ffa52ac6c55e99d6623d91
catpatnova <Oskar.Vaht@gmail.com>
2026-05-31 16:20:51 +0300

    Marquee added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,27 @@
 import '../index.css'
 import { Link } from 'react-router-dom'
+import { Button } from './ui/button'
+import { Marquee } from './ui/marquee'
 
 export function Footer() {
 
     return (
         <>
-            <div className='w-full bg-black text-white'>
-
+            <div className='bg-black text-white'>
+                <Marquee repeat={6} className='font-semibold w-screen'>
+                    <span>NEWSLETTER+++</span>
+                </Marquee>
                 <div className='mt-15 mx-10 gap-10'>
-                    <div className='flex-row items-center'>
+                    <div className='flex-row items-center justify-between'>
                         <h3 className='w-1/2'>DESIGN NEWS TO YOUR INBOX</h3>
-                        <div className='flex-row gap-2.5 h-fit'>
-                            <input type="email" placeholder='email' className='bg-white text-black py-3 px-4' />
-                            <button className='text-black bg-white p-2.5 '>
-                                <p>Sign up</p>
-                            </button>
+                        <div className='flex-row gap-2.5 h-fit items-center'>
+                            <input type="email" placeholder='E-Mail' className='bg-white text-black py-1 px-2' />
+                            <Button className='bg-white text-black rounded-none p-3 hover:bg-gray-200 text-[0.75rem]'>SIGN UP</Button>
                         </div>
                     </div>
 
                     <div className='flex-row justify-between'>
-                        <Link to="/">
+                        <Link className='h-fit' to="/">
                             <h3 className='text-2xl'>FYRRE MAGAZINE</h3>
                         </Link >
 
@@ -30,7 +32,13 @@ export function Footer() {
 
                             <div>
                                 <Link to="/Magazines">
-                                    Magazine
+                                    Magazines
+                                </Link >
+                                <Link to="/Podcasts">
+                                    Podcasts
+                                </Link >
+                                <Link to="/Authors">
+                                    Authors
                                 </Link >
                             </div>
 

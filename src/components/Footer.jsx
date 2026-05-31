729e9f01da3cb8d64bae3bdac961e0608268235c
catpatnova <Oskar.Vaht@gmail.com>
2026-05-31 15:29:53 +0300

    Shadcn added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,12 @@
 import '../index.css'
 import { Link } from 'react-router-dom'
-import Marquee from 'react-fast-marquee'
 
 export function Footer() {
 
     return (
         <>
             <div className='w-full bg-black text-white'>
-                <Marquee>
-                    <p>NEWSLETTER+++</p>
-                </Marquee>
+
                 <div className='mt-15 mx-10 gap-10'>
                     <div className='flex-row items-center'>
                         <h3 className='w-1/2'>DESIGN NEWS TO YOUR INBOX</h3>

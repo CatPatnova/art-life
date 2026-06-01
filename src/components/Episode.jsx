8f46966bcc2034d4b34a89fecc272befb892c693
catpatnova <Oskar.Vaht@gmail.com>
2026-06-01 17:14:15 +0300

    Podcasts page completed

---
+++ src/components/Episode.jsx
@@ -0,0 +1,31 @@
+import '../index.css'
+import { Link } from 'react-router-dom'
+
+export function Episode({ num, src, title, date, dur }) {
+    return (
+        <div className='flex-row items-center w-full justify-between py-15 border-b border-black'>
+            <div className='flex-row items-center gap-15'>
+                <h3>{num}</h3>
+                <img src={src} className='h-60' />
+                <h3>{title}</h3>
+            </div>
+            <div className='flex-row items-center gap-5'>
+                <div className='flex-row gap-2.5'>
+                    <p className='font-semibold'>Date</p>
+                    <p>{date}</p>
+                </div>
+                <div className='flex-row gap-2.5'>
+                    <p className='font-semibold'>Duration</p>
+                    <p>{dur}</p>
+                </div>
+                <Link to='/Podcasts/Post'>
+                    <div className='flex-row items-center'>
+                        <p className='font-semibold'>LISTEN</p>
+                        <img src="src/assets/arrow-right.svg" className='h-4 w-6 ' />
+                    </div>
+                </Link>
+            </div>
+
+        </div>
+    )
+}
--- src/pages/Podcasts.jsx
+++ src/pages/Podcasts.jsx
@@ -1,9 +1,45 @@
 import '../index.css'
+import { Episode } from '@/components/Episode'
 
 function Podcasts() {
     return (
-        <div>
+        <div className='items-center mt-10 mb-40 mx-15'>
             <h1>Podcasts</h1>
+            <Episode
+                num='05'
+                src='src/assets/podcast-cover1.svg'
+                title="The Problem of today's cultural development"
+                date='16. March 2022'
+                dur='1hr 20min'
+            />
+            <Episode
+                num='04'
+                src='src/assets/podcast-cover2.svg'
+                title="The hidden messages of Jack Nielson"
+                date='16. March 2022'
+                dur='1hr 20min'
+            />
+            <Episode
+                num='03'
+                src='src/assets/podcast-cover3.svg'
+                title="Behind the scenes of the street art culture"
+                date='16. March 2022'
+                dur='1hr 20min'
+            />
+            <Episode
+                num='02'
+                src='src/assets/podcast-cover4.svg'
+                title="The art of movement"
+                date='16. March 2022'
+                dur='1hr 20min'
+            />
+            <Episode
+                num='01'
+                src='src/assets/podcast-cover5.svg'
+                title="10 Artists you should definitely know"
+                date='16. March 2022'
+                dur='1hr 20min'
+            />
         </div>
     )
 }
